a45a548e2489bd79b2c30f2d81b189b812c53dfe
Rodolfo211192 <rodolfo_77_@hotmail.com>
2024-06-07 11:25:34 -0300

    Update main.py
    adicionei os códigos para adicionar e visualizar contatos

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print ("codigo para ser editado")
+contatos = {}
+
+def adicionar_contato(nome, telefone):
+    if nome in contatos:
+        print(f"O contato {nome} já existe.")
+    else:
+        contatos[nome] = telefone
+        print(f"Contato {nome} adicionado com sucesso.")
+
+def visualizar_contatos():
+    if contatos:
+        for nome, telefone in contatos.items():
+            print(f"Nome: {nome}, Telefone: {telefone}")
+    else:
+        print("Nenhum contato encontrado.")
+      
